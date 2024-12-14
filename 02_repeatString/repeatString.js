5f16b5b3b42d86436791b2a056ac6706a7a933a9
Mohamed Ayeb <93577912+mohamedayeb@users.noreply.github.com>
2024-12-14 20:42:53 +0100

    Edit repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let modifyString = '';
+    for (let i = 0; i < num; i += 1) {
+        modifyString += string;
+    }
+    return modifyString;
 };
 
 // Do not edit below this line
