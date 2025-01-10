62e7006824a666b482e0a2f6a6b4a4ceafad9be0
Mohamed Ayeb <93577912+mohamedayeb@users.noreply.github.com>
2025-01-10 20:01:47 +0100

    Edit sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    const isMinInvalid = min < 0 || !Number.isInteger(min);
+    const isMaxInvalid = max < 0 || !Number.isInteger(max);
+    if (isMinInvalid || isMaxInvalid) {
+        return 'ERROR';
+    }
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i += 1) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
